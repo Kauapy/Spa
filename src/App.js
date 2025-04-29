9acdfc7279321651a0f924f0ca2913409bb54021
Kauã Miranda <kauadecarvalho20@gmail.com>
2025-04-29 20:38:33 -0300

    Adicionar react-router-dom para roteamento e atualizar a estrutura do componente do aplicativo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import Inicio from './paginas/Inicio/index';
-import SobreMim from './paginas/SobreMim/index';
-
-console.log(window.location);
+import React from "react";
+import Inicio from "./paginas/Inicio/index";
+import SobreMim from "./paginas/SobreMim/index";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const pagina = window.location.pathname === "/" ? <Inicio /> : <SobreMim />;
-  return pagina;
+  return (
+    <BrowserRouter>
+      
+      <Routes>
+        <Route path="/" element={<Inicio></Inicio>}></Route>
+        <Route path="/sobremim" element={<SobreMim></SobreMim>}>
+          {" "}
+        </Route>
+        <Route path="*" element={<div>Page not found 404</div>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
