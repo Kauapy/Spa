c38f5a1106b8b5154c537456b4758ffc18f945b3
Kauã Miranda <kauadecarvalho20@gmail.com>
2025-04-29 20:23:42 -0300

    Refatorar a estrutura do aplicativo e remover arquivos não utilizados; adicionar componentes Inicio e SobreMim

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Inicio from './paginas/Inicio/index';
+import SobreMim from './paginas/SobreMim/index';
+
+console.log(window.location);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const pagina = window.location.pathname === "/" ? <Inicio /> : <SobreMim />;
+  return pagina;
 }
 
 export default App;
