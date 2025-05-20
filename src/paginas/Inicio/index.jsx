df790b4b796d8a1401b816b6a687b83adba1b6e2
Kauapy <kauadecarvalho20@gmail.com>
2025-05-19 22:26:37 -0300

    Remover componentes e estilos de banner não utilizados; atualizar arquivos jsconfig e package para consistência

--- src/paginas/Inicio/index.jsx
+++ src/paginas/Inicio/index.jsx
@@ -1,9 +1,8 @@
-import Banner from "../../componentes/Banner";
-
+import Teste from "../../componentes/Teste";
 export default function Inicio(){
     return(
         <main>
-            <Banner />
+            <Teste></Teste>
             <h1>Hello World</h1>
         </main>
     )
---
+++ src/paginas/componentes/Teste/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Teste = () => {
+  return (
+    <div>
+        <h2>dahdshja</h2>
+    </div>
+  )
+}
+
+export default Teste;
